f3ed90d6684c35160bc82f2e052907b7837073e6
BeaconBrigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-04-24 23:02:30 -0230

    Added head snake that moves around. Follower snakes don't work.

--- second-attempt-sfml/main.cpp
+++ second-attempt-sfml/main.cpp
@@ -16,45 +16,34 @@
 
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-
-// Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
+#include <iostream>
+#include "snake.hpp"
 
-int main(int, char const**)
+const float BLOCK_WIDTH = 20.0f;
+
+const sf::Vector2<float> UP (0.f, -BLOCK_WIDTH / 2);
+const sf::Vector2<float> DOWN (0.f, BLOCK_WIDTH / 2);
+const sf::Vector2<float> LEFT (-BLOCK_WIDTH / 2, 0.f);
+const sf::Vector2<float> RIGHT (BLOCK_WIDTH / 2, 0.f);
+
+void pause(sf::RenderWindow*);
+
+int main()
 {
+    bool hasChangedDirection = false, doneMoving = true;
+    sf::Vector2f toChange(0.f, 0.f);
+    
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Snake Game!");
+    window.setFramerateLimit(30);
+    
+    // head snake
+    Snake head(true);
+    head.m_Sprite.setColor(sf::Color(255, 0, 0));
+    Snake follow1(false);
+    follow1.m_Sprite.setColor(sf::Color(0, 255, 0));
+    Snake follow2(false);
 
     // Start the game loop
     while (window.isOpen())
@@ -63,29 +52,132 @@ int main(int, char const**)
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
+            switch (event.type)
+            {
+                // Close window: exit
+                case sf::Event::Closed :
+                    window.close();
+                    break;
+                // handle directional keys pressed
+                case sf::Event::KeyPressed :
+                    
+                    if (event.key.code == sf::Keyboard::Escape)
+                        pause(&window);
+        
+                    if (event.key.code == sf::Keyboard::W)
+                    {
+                        if (head.m_HeadDirection != DOWN && !hasChangedDirection)
+                        {
+                            toChange = UP;
+                            hasChangedDirection = true;
+                        }
+                    }
+                    else if (event.key.code == sf::Keyboard::S)
+                    {
+                        if (head.m_HeadDirection != UP && !hasChangedDirection)
+                        {
+                            toChange = DOWN;
+                            hasChangedDirection = true;
+                        }
+                    }
+                    else if (event.key.code == sf::Keyboard::A)
+                    {
+                        if (head.m_HeadDirection != RIGHT && !hasChangedDirection)
+                        {
+                            toChange = LEFT;
+                            hasChangedDirection = true;
+                        }
+                    }
+                    else if (event.key.code == sf::Keyboard::D)
+                    {
+                        if (head.m_HeadDirection != LEFT && !hasChangedDirection)
+                        {
+                            toChange = RIGHT;
+                            hasChangedDirection = true;
+                        }
+                    }
+                    break;
+                default :
+                    break;
             }
         }
-
+        hasChangedDirection = false;
+            
         // Clear screen
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
+        
+        // update all following snake positions
+        for (int i = head.m_SnakeList.size() - 1; i > 0; i--)
+        {
+            head.m_SnakeList[i].m_Sprite.setPosition(head.m_SnakeList[i - 1].m_Sprite.getPosition());
+            window.draw(head.m_SnakeList[i].m_Sprite);
+        }
+        
+        // update head snake
+        if (doneMoving)
+            head.m_HeadDirection = toChange;
+        head.m_Sprite.move(head.m_HeadDirection);
+        window.draw(head.m_Sprite);
+        doneMoving = !doneMoving;
+        
         window.display();
     }
 
     return EXIT_SUCCESS;
+}
+
+void pause(sf::RenderWindow* window)
+{
+    sf::Text countdown;
+    sf::Font font;
+    if (!font.loadFromFile("ARCAC___.TTF"))
+        exit(1);
+    countdown.setFont(font);
+    countdown.setCharacterSize(40);
+    countdown.setString("Press any key to continue");
+    countdown.setPosition(sf::Vector2f(100, 375));
+    window->clear();
+    window->draw(countdown);
+    window->display();
+    
+    sf::Event event;
+    while (window->isOpen())
+    {
+        window->waitEvent(event);
+        if (event.type == sf::Event::KeyPressed)
+        {
+            // display countdown before starting again
+            countdown.setPosition(sf::Vector2f(375, 375));
+            countdown.setString("3");
+            window->clear();
+            window->draw(countdown);
+            window->display();
+            
+            sf::sleep(sf::seconds(1.f));
+            countdown.setString("2");
+            window->clear();
+            window->draw(countdown);
+            window->display();
+            
+            sf::sleep(sf::seconds(1.f));
+            countdown.setString("1");
+            window->clear();
+            window->draw(countdown);
+            window->display();
+            
+            sf::sleep(sf::seconds(1.f));
+            countdown.setString("GO!");
+            window->clear();
+            window->draw(countdown);
+            window->display();
+            sf::sleep(sf::seconds(1.f));
+            
+            return;
+        }
+        if (event.type == sf::Event::Closed)
+        {
+            window->close();
+            return;
+        }
+    }
 }
